f12dca465b569043a61d617e6870d51ce058de0d
popododo0720 <popododo0720@naver.com>
2026-08-05 16:26:57 +0900

    edit nvim config: add blink , treesitter

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,8 +1,31 @@
 { pkgs, ... }:
+let
+  ts = pkgs.vimPlugins.nvim-treesitter.withPlugins (
+    p: with p; [
+      bash
+      python
+      nix
+      yaml
+      lua
+      json
+      markdown
+      markdown_inline
+      vim
+      vimdoc
+      regex
+      toml
+      html
+      css
+    ]
+  );
+in
 {
   home.packages = with pkgs; [
     neovim
   ];
+
+  xdg.dataFile."nvim/site/pack/nix/start/nvim-treesitter".source = ts;
+  xdg.dataFile."nvim/site/pack/nix/start/blink-cmp".source = pkgs.vimPlugins.blink-cmp;
 
   home.sessionVariables = {
     EDITOR = "nvim";
